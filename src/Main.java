35e985a3b82911879ef1df3fc3dba581b7c4b807
Miguel <miguelarodriguez7594@gmail.com>
2019-09-03 11:51:48 -0500

    Created Main class, and tests to see if the Triangle is valid

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Tri.Triangle;
+
+public class Main {
+    public static void main(String []args){
+        Triangle testTri = new Triangle();
+        testTri.setSides(3,4,5);
+        System.out.println(testTri);
+    }
+}
